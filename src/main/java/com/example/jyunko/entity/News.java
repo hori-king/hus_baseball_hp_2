7d4522b812466f2cfd268bc17acd8b57f238a9fc
kouki <kuhuangqi758@gmail.com>
2025-08-22 00:16:54 +0900

    新規作成

---
+++ src/main/java/com/example/jyunko/entity/News.java
@@ -0,0 +1,25 @@
+package com.example.jyunko.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "news")
+@Data
+public class News {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private LocalDate postedDate;
+	private String title;
+	private String content;
+	private String category;
+
+}
