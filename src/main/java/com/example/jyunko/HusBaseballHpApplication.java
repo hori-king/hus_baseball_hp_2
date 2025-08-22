ba2985efd9d2ea4bcb4951d3248001da2ae2fb02
kouki <kuhuangqi758@gmail.com>
2025-08-22 23:24:17 +0900

    パスの変更

--- src/main/java/com/example/jyunko/HusBaseballHpApplication.java
+++ src/main/java/com/example/jyunko/HusBaseballHpApplication.java
@@ -1,12 +1,17 @@
-package com.example.demo;
+package com.example.jyunko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class HusBaseballHpApplication {
 
 	public static void main(String[] args) {
+		// .envファイルの内容を環境変数として読み込む
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
 		SpringApplication.run(HusBaseballHpApplication.class, args);
 	}
 
