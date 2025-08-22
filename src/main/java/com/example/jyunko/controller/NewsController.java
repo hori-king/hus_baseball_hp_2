88ed1a644f1dcc8c575fec4c5dc1411a076d9e66
kouki <kuhuangqi758@gmail.com>
2025-08-22 23:26:59 +0900

    新規作成

---
+++ src/main/java/com/example/jyunko/controller/NewsController.java
@@ -0,0 +1,25 @@
+package com.example.jyunko.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.jyunko.entity.News;
+import com.example.jyunko.service.NewsService;
+
+@Controller
+public class NewsController {
+	@Autowired
+	private NewsService newsService;
+	
+	@GetMapping("/news")
+	public String listNews(Model model) {
+		List<News> newsList = newsService.findAll();
+		model.addAttribute("newsList", newsList);
+		return "news";
+	}
+
+}
