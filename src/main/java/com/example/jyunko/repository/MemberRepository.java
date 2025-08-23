6f68ac44b197a050c916773afb6dd7d19ad08253
kouki <kuhuangqi758@gmail.com>
2025-08-23 23:57:58 +0900

    新規作成

---
+++ src/main/java/com/example/jyunko/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.example.jyunko.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.jyunko.entity.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+
+}
