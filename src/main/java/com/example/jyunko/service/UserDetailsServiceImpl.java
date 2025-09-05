e164890fa67bb91dba7440e1ff578da5752b9499
kouki <kuhuangqi758@gmail.com>
2025-09-05 23:57:02 +0900

    新規作成

---
+++ src/main/java/com/example/jyunko/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.jyunko.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.jyunko.entity.User;
+import com.example.jyunko.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+
+		return new org.springframework.security.core.userdetails.User(
+				user.getUsername(),
+				user.getPassword(),
+				AuthorityUtils.createAuthorityList("ROLE_" + user.getRole()));
+	}
+}
