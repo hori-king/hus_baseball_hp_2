260d898fc36f0330a80f5113ee5d584d1c2b51cf
kouki <kuhuangqi758@gmail.com>
2025-09-05 22:30:19 +0900

    .envファイルの読み込み方法の変更

--- src/main/java/com/example/jyunko/HusBaseballHpApplication.java
+++ src/main/java/com/example/jyunko/HusBaseballHpApplication.java
@@ -11,7 +11,10 @@ public class HusBaseballHpApplication {
 	public static void main(String[] args) {
 		// .envファイルの内容を環境変数として読み込む
 		Dotenv dotenv = Dotenv.load();
-		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(HusBaseballHpApplication.class, args);
 	}
 
