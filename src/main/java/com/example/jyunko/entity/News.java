ca7bec2753e18a9293b458b258efd257d2b53ba8
kouki <kuhuangqi758@gmail.com>
2025-09-12 00:15:36 +0900

    掲載日のデータベースの列名を変更 日にちのフォーマット固定

--- src/main/java/com/example/jyunko/entity/News.java
+++ src/main/java/com/example/jyunko/entity/News.java
@@ -2,11 +2,14 @@ package com.example.jyunko.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
@@ -17,6 +20,8 @@ public class News {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name = "posted_date")
 	private LocalDate postedDate;
 	private String title;
 	private String content;
