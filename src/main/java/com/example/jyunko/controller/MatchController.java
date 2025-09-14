7574ba4257692896923f1f47877783f4d1577681
kouki <kuhuangqi758@gmail.com>
2025-09-14 22:18:11 +0900

    管理者画面と新規登録のロジックを追加

--- src/main/java/com/example/jyunko/controller/MatchController.java
+++ src/main/java/com/example/jyunko/controller/MatchController.java
@@ -2,11 +2,17 @@ package com.example.jyunko.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.jyunko.entity.Match;
 import com.example.jyunko.service.MatchService;
@@ -29,4 +35,30 @@ public class MatchController {
 		model.addAttribute("match", match);
 		return "match_detail";
 	}
+
+	@GetMapping("/admin/matches")
+	public String showMatches(Model model) {
+		List<Match> matchList = matchService.findAll();
+		model.addAttribute("matchList", matchList);
+		return "admin/matches/list";
+	}
+
+	@GetMapping("/admin/matches/new")
+	public String matchForm(Model model) {
+		model.addAttribute("match", new Match());
+		return "admin/matches/form";
+	}
+
+	@PostMapping("/admin/matches")
+	public String createMatch(@ModelAttribute @Valid Match match, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			redirectAttributes.addFlashAttribute("errorMessage", "エラーが発生しました。");
+			return "admin/matches/form";
+		}
+		matchService.save(match);
+		redirectAttributes.addFlashAttribute("successMessage", "試合結果を登録しました。");
+		return "redirect:/admin/matches";
+	}
+
 }
